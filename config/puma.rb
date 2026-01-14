8b400818130c6f91757a531d3c96aeab462a338f
rayees1riyaz <rayeesriyaz343@gmail.com>
2026-01-14 12:45:28 +0530

    Fix Puma double bind on Render

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,6 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-port ENV.fetch("PORT", 3000)
 bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 
 plugin :tmp_restart
